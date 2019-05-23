dcc597d6f0a53f61d3e0a7746fac30a7749e5ab2
Ligay <gesh-tal@yandex.kz>
2019-05-23 14:09:22 +0600

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,14 @@
+
+// $(document).ready(function(){
+// 	var $filter = $('.filter_block').isotope({
+// 		// itemSelector: '.filter_item',
+// 		// lalyoutMode: 'fitRows'
+// 		// filter: '.best_item'
+// 	});
+// 	$('.filter_buttons').on('click', 'a', function(){
+// 		var filterValue = $(this).attr('data-filter');
+// 		$filter.isotope({
+// 			filter: filterValue
+// 		});
+// 	});
+// });
